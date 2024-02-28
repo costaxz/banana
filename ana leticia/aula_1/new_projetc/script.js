aebdcadf6afb6da966b0dad1b7708db4e6a8d4c1
Ana Banana <161311234+costaxz@users.noreply.github.com>
2024-02-28 12:22:31 -0300

    Add files via upload

---
+++ ana leticia/aula_1/new_projetc/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function () {
+    const decreaseFontSizeButton= document.getElementById(decreaseFontSizeButton);
+        const inscreseFontSizeButton= document.getElementById(decreaseFontSizeButton);
+        const chagecolorButton = document.getElementById(decreaseFontSizeButton);
+        let currentfontsize =10;//tamanhoda fonte inicial
+        let colorschag
+
+
+}
