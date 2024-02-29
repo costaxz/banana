d33f00690f664bd48ccebad994d8005f877318b6
Ana Banana <161311234+costaxz@users.noreply.github.com>
2024-02-29 09:46:21 -0300

    Delete ana leticia/aula_1/new_projetc directory

--- ana leticia/aula_1/new_projetc/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded",function () {
-    const decreaseFontSizeButton= document.getElementById(decreaseFontSizeButton);
-        const inscreseFontSizeButton= document.getElementById(decreaseFontSizeButton);
-        const chagecolorButton = document.getElementById(decreaseFontSizeButton);
-        let currentfontsize =10;//tamanhoda fonte inicial
-        let colorschag
-
-
-}
